315238281e6548d864670daba12ce3726619d2ab
Adam Hodges <adam@ajhodges.com>
2015-06-16 20:02:13 -0400

    Decrease GPS update interval 3

--- app/src/main/java/com/codeforgvl/trolleytracker/Constants.java
+++ app/src/main/java/com/codeforgvl/trolleytracker/Constants.java
@@ -5,7 +5,7 @@ public final class Constants {
     // Milliseconds per second
     private static final int MILLISECONDS_PER_SECOND = 1000;
     // Update frequency in seconds
-    private static final int UPDATE_INTERVAL_IN_SECONDS = 4;
+    private static final int UPDATE_INTERVAL_IN_SECONDS = 3;
     // Update frequency in milliseconds
     public static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
 
