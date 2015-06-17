4794f37ac97aa3ecc2e25def1746e9bbfd270320
Adam Hodges <adam@ajhodges.com>
2015-06-16 20:16:17 -0400

    Properly start service on boot, fixes #3

--- app/src/main/java/com/codeforgvl/trolleytracker/LocationLoggerServiceManager.java
+++ app/src/main/java/com/codeforgvl/trolleytracker/LocationLoggerServiceManager.java
@@ -5,34 +5,19 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 public class LocationLoggerServiceManager extends BroadcastReceiver {
-
-    private SharedPreferences mPrefs;
     public static final String TAG = "LocationLoggerServiceManager";
     @Override
     public void onReceive(Context context, Intent intent) {
         // Make sure we are getting the right intent
         if( "android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
-            boolean mUpdatesRequested = false;
-            // Open the shared preferences
-            mPrefs = context.getSharedPreferences("SharedPreferences",
-                    Context.MODE_PRIVATE);
-	        /*
-	         * Get any previous setting for locationInfo updates
-	         * Gets "false" if an error occurs
-	         */
-            if (mPrefs.contains("KEY_UPDATES_ON")) {
-                mUpdatesRequested = mPrefs.getBoolean("KEY_UPDATES_ON", false);
-            }
-            if(mUpdatesRequested){
-                ComponentName comp = new ComponentName(context.getPackageName(), BackgroundLocationService.class.getName());
-                ComponentName service = context.startService(new Intent().setComponent(comp));
+            boolean mUpdatesRequested = PreferenceManager.getInstance().getBackgroundTestsEnabled(context);
 
-                if (null == service){
-                    // something really wrong here
-                    //Log.e(TAG, "Could not start service " + comp.toString());
-                }
+            if(mUpdatesRequested){
+                Intent mi=new Intent(context, BackgroundLocationService.class);
+                context.startService(mi);
             }
 
         } else {
